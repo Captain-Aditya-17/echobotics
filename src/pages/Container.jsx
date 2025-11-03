5372006c6bfae07d96d4e048b769a9e42c46e7fb
Captain-Aditya-17 <khushimate90@gmail.com>
2025-11-03 19:42:39 +0530

    forth

--- src/pages/Container.jsx
+++ src/pages/Container.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from './Home'
 import Navbar from '../components/Navbar'
-import dust from "../assets/dust.png";
 import About from './About';
 
 
@@ -9,7 +8,6 @@ import About from './About';
 const Container = () => {
   return (
     <div className='w-full bg-black text-white relative overflow-hidden'>
-            <img className="absolute left-0 top-0 w-full" src={dust} alt="" />
         <Navbar />
         <Home/>
             <About/>
